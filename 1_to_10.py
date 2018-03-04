a236f0489d223a35c4c4360a30f1746a21775f63
Jared Stevens <jared.b.stevens@gmail.com>
2018-03-04 03:45:54 +0000

    updated to only use while loop

--- 1_to_10.py
+++ 1_to_10.py
@@ -1,14 +1,4 @@
-for number in range(1, 11):
-    print(number)
-
-print('')
-
 count = 1
 while count <= 10:
     print(count)
     count += 1
-
-print('')  
-
-for number in range(1, 11, 1):
-    print(number)
