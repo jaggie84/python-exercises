fab5a132134b29bb904770b043d64b37834057d7
Jared Stevens <jared.b.stevens@gmail.com>
2018-03-04 03:40:12 +0000

    Coins Exercise

---
+++ coins.py
@@ -0,0 +1,11 @@
+num_coins = 0
+print("You have {} coins.".format(num_coins))
+while True:
+    switch = input("Do you want another? (yes/no) ")
+    if switch == 'yes':
+        num_coins += 1
+        print("You have {} coins.".format(num_coins))
+    elif switch == 'no':
+        print("Bye")
+        break
+    
