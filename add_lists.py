598f89380d9dbf9fc2d7c54851b178254d784d2a
Jared Stevens <jared.b.stevens@gmail.com>
2018-03-06 03:09:54 +0000

    testing variables

--- add_lists.py
+++ add_lists.py
@@ -1,10 +1,11 @@
 arr = [2, 4, 5]
 arr2 = [2, 3, 6]
+arr3 = [1, 1, 1]
 
-def addVectors(list, list2):
+def addVectors(list, list2, list3):
     addList = []
     for i in range(0, len(list)):
-        addList.append(list[i] + list2[i])    
+        addList.append(arr[i] + arr2[i] + arr3[i])    
     return addList
 
-print(addVectors(arr, arr2))
+print(addVectors(arr, arr2, arr3))
