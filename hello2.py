ddf0cdf3aeea4371b3ebd5c5ad774c7c8482e3fe
Jared Stevens <jared.b.stevens@gmail.com>
2018-03-10 15:47:46 +0000

    updates

--- hello2.py
+++
@@ -1,4 +0,0 @@
-name = input("What is your name? ")
-print("Hello,".upper(),name.upper())
-total = len(name)
-print("Your name has".upper(), total, "letters in it!".upper())
--- multiply_a_list.py
+++
@@ -1,4 +0,0 @@
-list1 = [2, 4, 6]
-x = 2
-product = [x * list1]
-print(product)
