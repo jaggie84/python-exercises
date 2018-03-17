10a41891c6ff6bc1f078c1199ae08ff347994376
Jared Stevens <jared.b.stevens@gmail.com>
2018-03-17 08:41:16 -0500

    Delete .~c9_invoke_G0hTYs.py

--- .~c9_invoke_G0hTYs.py
+++
@@ -1,7 +0,0 @@
-day = int(input('Day (0-6)? '))
-days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-
-if day 
-  print("Go to Work.")
-else:
-    print("Sleep in!")
