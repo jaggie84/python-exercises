71499446bbc8854080cf6f806231371b2ad145fe
Jared Stevens <jared.b.stevens@gmail.com>
2018-03-17 18:26:32 +0000

    udpated data.pickle and 1st File IO excercise

---
+++ File_IO.1.py
@@ -0,0 +1,9 @@
+f = input("\n Hello, Please enter the file name you'd like to open: ")
+file_handle = open(f, 'r') 
+while True:
+  char = file_handle.read()
+  if not char:
+    break
+  else:
+    print(char)
+file_handle.close()
