2531193ff51cd38597d5e7fc10ccd49e4ea6b878
Jared Stevens <jared.b.stevens@gmail.com>
2018-03-20 23:17:11 +0000

    updated class Person with all print methods

--- objects_bascis.py
+++ objects_bascis.py
@@ -5,9 +5,14 @@ class Person:
          self.phone = phone
      def greet(self, other_person):
          print('Hello {}, I am {}!'.format(other_person.name, self.name))
+     def print_contact_info(self):
+         print("{}'s email is: {}, and {}'s phone number is: {}".format(self.name, self.email, self.name, self.phone))
 sonny = Person('Sonny', 'test@gmail.com', '713-584-3669')
 jordan = Person('Jordan', 'test2@gmail.com', '123-456-7890')
 sonny.greet(jordan)
 jordan.greet(sonny)
 print(sonny.name, sonny.email, sonny.phone)
 print(jordan.name, jordan.email, jordan.phone)
+sonny.print_contact_info()
+jordan.print_contact_info()
+
