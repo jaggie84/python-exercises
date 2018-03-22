f5aba9c9db2219a258218457960f841ba58a4f6b
Jared Stevens <jared.b.stevens@gmail.com>
2018-03-22 23:20:44 +0000

    Objects

--- objects_basics.1.py
+++ objects_basics.1.py
@@ -4,7 +4,7 @@ class Vehicle:
          self.model = model
          self.year = year
      def print_info(self):
-         print('My car was made by {}, the model is a {}, and it was produced in {}!'.format(self.make, self.model, self.year))
+         print('Car make: {} //Car Model: {} //Car Production Year: {}.'.format(self.make, self.model, self.year))
 car = Vehicle('Nissan', 'Leaf', '2015')
 print(car.make, car.model, car.year)
 car.print_info()
